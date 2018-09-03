962442186a437eca793dfa78b7145375318ded81
calmkart <calmkart@calmkart.com>
2018-09-03 17:07:14 +0800

    fix bug

--- rm_timer.py
+++ rm_timer.py
@@ -9,12 +9,13 @@ import os
 import time
 
 def main(n):
-    path = "/home/shared/upload/"
-    files = os.listdir(path)
-    for file in files:
-        t = os.path.getctime(path+file)
-        if time.time() - t > 604800:
-            os.remove(path+file)
-    time.sleep(n)
+    while 1:
+        path = "/home/shared/upload/"
+        files = os.listdir(path)
+        for file in files:
+            t = os.path.getctime(path+file)
+            if time.time() - t > 604800:
+                os.remove(path+file)
+        time.sleep(n)
 
 main(86400)
